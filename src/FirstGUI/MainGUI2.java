71fa056751d785292ea37a3d8dadcd4d441f2a25
Christopher G Gonzalez <christopherggonzalez@Christophers-MacBook-Air.local>
2023-12-19 21:55:44 -0800

    error bugs

--- src/FirstGUI/MainGUI2.java
+++ src/FirstGUI/MainGUI2.java
@@ -1,16 +1,20 @@
 package FirstGUI;
 
-
+import java.util.Stack;
 
 public class MainGUI2 extends Application {
-
+	@Override
+	public void start(Stage primaryStage) throws Exception { 
+		StackPane root = new StackPane();
+		scene scene = new Scene(root, 300, 200);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("javaFX window");
+		primaryStage.show();
+	}
 	public static void main(String[] args) {
 		launch(args);
 
 	}
-	@Override
-	public void start(stage primaryStage) throws Exception { 
+
 		
 	}
-
-}
