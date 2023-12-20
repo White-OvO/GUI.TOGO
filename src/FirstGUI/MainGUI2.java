034090f3286dbba169e6b35abe2b0f541ace7653
Christopher G Gonzalez <christopherggonzalez@Christophers-MacBook-Air.local>
2023-12-19 21:48:09 -0800

    new project

---
+++ src/FirstGUI/MainGUI2.java
@@ -0,0 +1,16 @@
+package FirstGUI;
+
+
+
+public class MainGUI2 extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+	@Override
+	public void start(stage primaryStage) throws Exception { 
+		
+	}
+
+}
