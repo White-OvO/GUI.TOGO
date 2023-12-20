0bc2feee7515e000fbc16d1d31944134d9eeff7f
Christopher G Gonzalez <christopherggonzalez@Christophers-MacBook-Air.local>
2023-12-19 20:28:20 -0800

    Button implemented

--- src/FirstGUI/GUI.java
+++ src/FirstGUI/GUI.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -12,20 +13,29 @@ public class GUI {
 
 
 
-	    public static void main(String[] args) {
+	    public GUI(){
 	        JFrame frame = new JFrame();
 	        JPanel panel = new JPanel();
+	        JButton button = new JButton("Click here");    // cretes the button (1/2)
+	        
 	        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
 	        panel.setLayout(new GridLayout(0, 1));
+	        panel.add(button); // adds the button (2/2)
+	        
+	        
 	        frame.add(panel, BorderLayout.CENTER);
 	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        frame.setTitle("Our GUI");
+	        frame.setTitle("Our GUI");   //title
 	        frame.pack();
 	        frame.setVisible(true);
 	    
 
 			
 		}
+
+	    public static void main(String[] args) {
+	    	new GUI(); // creates GUI  adject from GUI CLASS
+	    }
 
 	}
 
