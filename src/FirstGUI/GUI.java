c60ee079787b3e46af93e80a05980cf132fbc1e3
Christopher G Gonzalez <christopherggonzalez@Christophers-MacBook-Air.local>
2023-12-19 20:27:39 -0800

    button

--- src/FirstGUI/GUI.java
+++ src/FirstGUI/GUI.java
@@ -9,8 +9,8 @@ import javax.swing.JPanel;
 
 public class GUI {
 
-	//
-<<<<<<< HEAD
+
+
 
 	    public static void main(String[] args) {
 	        JFrame frame = new JFrame();
@@ -23,49 +23,8 @@ public class GUI {
 	        frame.pack();
 	        frame.setVisible(true);
 	    
-=======
-		public GUI() {
-			JFrame frame = new JFrame();
+
 			
-			JPanel panel = new JPanel();
-			panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-			panel.setLayout(new GridLayout(0, 1));
-			
-			
-			frame.add(panel, BorderLayout.CENTER);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setTitle("Our GUI");
-			frame.pack();
-			frame.setVisible(true);
-
-
-
-			/*
-
-
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-public class GUI {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0, 1));
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Our GUI");
-        frame.pack();
-        frame.setVisible(true);
-    }
-}
-
-
-			*/
->>>>>>> 7fda0025418aef83651f1ccbd755db956149e25e
 		}
 
 	}
