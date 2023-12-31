f6fd84868f7d45cbacd1689e0a6952635060c11b
Christopher G Gonzalez <christopherggonzalez@Christophers-MacBook-Air.local>
2023-12-31 02:05:22 -0800

    gui first and last name

--- src/FirstGUI/GUI.java
+++ src/FirstGUI/GUI.java
@@ -1,5 +1,4 @@
 package FirstGUI;
-
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -10,49 +9,57 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class GUI implements ActionListener{
-		private int count = 0;
-		private JLabel label;
-		private JFrame frame;
-		private JPanel panel;
-		/// private methods for the gui in this class
-
-	    public GUI(){
-	        JFrame frame = new JFrame();
-	        JPanel panel = new JPanel();
-	        JButton button = new JButton("Click here");    // cretes the button (1/2)
-	        label = new JLabel("Number of clicks: 0");   // label quote
-	        button.addActionListener(this);// number of clicks to go up
-	        
-	        panel = new JPanel();
-	        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-	        panel.setLayout(new GridLayout(0, 1));
-	        panel.add(button); // adds the button (2/2)
-	        panel.add(label); // displays label on bottom (2/2)
-	        
-	        frame.add(panel, BorderLayout.CENTER);
-	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        frame.setTitle("Our GUI");   //title
-	        frame.pack();
-	        frame.setVisible(true);
-	    
-
-			
-		}
-
-	    public static void main(String[] args) {
-	    	new GUI(); // creates GUI  adject from GUI CLASS
-	    }
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-			count++;
-			label.setText("Number of clicks: " + count); // increases number of clicks
-			
-		}
-
+	// to add a first and last name we need to add a JLabel and JPannel
+	private int count = 0;
+	private JLabel label;
+	private JFrame frame;
+	private JPanel panel;
+	private JTextField firstNameField;
+	private JTextField lastNameField;
+	
+	public GUI(){
+		frame = new JFrame();
+		panel = new JPanel();
+		JButton button = new JButton("Click here");   
+		label = new JLabel("Number of clicks: 0");   
+		button.addActionListener(this);
+		
+		panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+		panel.setLayout(new GridLayout(0, 1));
+		panel.add(button); 
+		panel.add(label); 
+		
+		JPanel namePanel = new JPanel();
+		namePanel.setLayout(new GridLayout(0, 2));
+		
+		firstNameField = new JTextField();
+		lastNameField = new JTextField();
+		
+		namePanel.add(new JLabel("First Name:"));
+		namePanel.add(firstNameField);
+		namePanel.add(new JLabel("Last Name:"));
+		namePanel.add(lastNameField);
+		
+		panel.add(namePanel);
+		
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Our GUI");  
+		frame.pack();
+		frame.setVisible(true);
 	}
 
+	public static void main(String[] args) {
+		new GUI(); 
+	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		count++;
+		label.setText("Number of clicks: " + count); 
+	}
+}
